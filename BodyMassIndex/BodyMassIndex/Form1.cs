08ed127a30273dae67736b6a30cff0b3076ce5e4
PhuHung <hung.187pm06602@vanlanguni.vn>
2019-11-07 17:32:24 +0700

    BMI

---
+++ BodyMassIndex/BodyMassIndex/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BodyMassIndex
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.button1.Click += button1_Click;
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double Height = double.Parse(txtH.Text);
+            double Weight = double.Parse(txtW.Text);
+            double Result = Weight / ((Height / 100) * (Height / 100));
+            Result = Math.Round(Result, 1);
+            txtBMI.Text = Result.ToString();
+            
+
+        }
+    }
+}
